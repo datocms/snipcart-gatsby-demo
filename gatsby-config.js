9fdea09a883948b6bb88c2ad1dda9332e9924039
Irene Oppo <i.oppo@datocms.com>
2022-10-24 12:35:13 +0200

    Update env

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,16 @@ module.exports = {
     'gatsby-plugin-sass',
     {
       resolve: `gatsby-source-datocms`,
-      options: { apiToken: process.env.DATO_API_TOKEN },
+      options: {
+        apiToken:
+          process.env.DATO_API_TOKEN || '1b2fe1bfc8abf2e08343a75e444b4d',
+      },
     },
     {
       resolve: 'gatsby-plugin-snipcart',
       options: {
         apiKey:
+          process.env.SNIPCART_PUBLIC_TOKEN ||
           'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
         autopop: true,
       },
